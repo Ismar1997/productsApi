d7e5fce6faa450edca5b08b960955e17f091489b
Ismar1997 <ismarfazlic5@gmail.com>
2021-09-26 20:39:07 +0200

    Change db credentials

--- config/Database.php
+++ config/Database.php
@@ -1,10 +1,10 @@
 <?php 
   class Database {
     // DB Params
-    private $host = 'localhost';
-    private $db_name = 'id17615045_products';
-    private $username = 'id17615045_ismar';
-    private $password = 'b6e?q]kin[YATg#e';
+    private $host = 'eu-cdbr-west-01.cleardb.com';
+    private $db_name = 'heroku_b7331217180f128';
+    private $username = 'b825a4ff8a361a';
+    private $password = '6f502ec0';
     private $conn;
 
     // DB Connect
